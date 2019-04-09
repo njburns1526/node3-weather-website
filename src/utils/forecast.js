883fd31ab98ac96e11193b9d869d2c0f8e6106b9
Nicholas Burns <njburns1526@gmail.com>
2019-04-08 21:41:02 -0400

    Add uv Index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, lon, callback) => {
     } else if (body.error) {
         callback('Unable to find location. Try another location', undefined)
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' )
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The UV index is ' + body.currently.uvIndex + '.' )
     }
 })
 }
